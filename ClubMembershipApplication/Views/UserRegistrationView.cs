7ca502d04875ae50111f802986ee7191d400fe2e
Kuba <jacob.tulacz@gmail.com>
2024-10-09 20:48:02 +0200

    Add changes to the proj

---
+++ ClubMembershipApplication/Views/UserRegistrationView.cs
@@ -0,0 +1,59 @@
+﻿using ClubMembershipApplication.Data;
+using ClubMembershipApplication.FieldValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Views
+{
+    public class UserRegistrationView:IView
+    {
+        IFieldValidator _fieldValidator = null;
+
+        IRegister _register = null;
+
+        public IFieldValidator FieldValidator { get => _fieldValidator; }
+
+        public UserRegistrationView(IRegister register, IFieldValidator fieldValidator)
+        {
+            _fieldValidator = fieldValidator;
+            _register = register;
+        }
+
+        public void RunView()
+        {
+          
+        }
+
+        private string GetInputFromUser(FieldConstants.UserRegistrationField field, string promptText)
+        {
+            string fieldVal = "";
+
+            do
+            {
+                Console.WriteLine(promptText);
+                fieldVal = Console.ReadLine();
+
+            } while(!FieldValid(field, fieldVal));
+
+            return fieldVal;
+;       }
+
+        private bool FieldValid(FieldConstants.UserRegistrationField field, string fieldValue)
+        {
+            if(_fieldValidator.ValidatorDel((int)field, fieldValue, _fieldValidator.FieldArray, out string invalidMessage))
+            {
+                CommonOutputFormat.ChangeFontColor(FontTheme.Danger);
+
+                Console.WriteLine(invalidMessage);
+
+                CommonOutputFormat.ChangeFontColor(FontTheme.Default);
+
+                return false;
+            }    
+            return true;
+        }
+    }
+}
